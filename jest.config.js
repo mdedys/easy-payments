61f7d4e73b326785773137cd1c7e98a7e1335bc7
Mike Dedys <mikededys@gmail.com>
2021-04-11 21:09:17 -0400

    Add react webapp

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  transform: {
-    '^.+\\.ts?$': 'ts-jest'
-  },
-  testEnvironment: 'node',
-  testRegex: './src/.*\\.(test|spec)?\\.(ts|ts)$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-}
